de424a380b47c650cdab0007d922ab89f423a9e4
Tommy <mckeont@upenn.edu>
2025-01-31 20:04:50 -0500

    Add files via upload

---
+++ phillythrive1.js
@@ -0,0 +1,386 @@
+var phillythrive1 = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1973054312356,
+                    39.9372151214386
+                ]
+            },
+            "properties": {
+                "UID": 1,
+                "Address": null,
+                "Name": "Stinger Square ",
+                "Category/Layer": "Public Space/Parks ",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1dNzuZOfUVoMCuhvpGpKsgnGnJRMiMV7I",
+                    "https://drive.google.com/uc?export=view&id=1_iXVENWxG8cVcXAOwXDLmvsHHmQGnp0T"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1937744328238,
+                    39.9336118136954
+                ]
+            },
+            "properties": {
+                "UID": 2,
+                "Address": null,
+                "Name": "Lanier Playground ",
+                "Category/Layer": "Public Space/Parks",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1GBrE2ad5hn1LoD63kU6fOBScQeNGzsr6",
+                    "https://drive.google.com/uc?export=view&id=1_Qdhhvs9PG06oV79QXP7TZCeqJMYUwKc"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.192326427974,
+                    39.937208118814
+                ]
+            },
+            "properties": {
+                "UID": 3,
+                "Address": "1308 S 29th",
+                "Name": "Peace Wall",
+                "Category/Layer": "Public Space/Parks",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": []
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1976770458644,
+                    39.9329213240905
+                ]
+            },
+            "properties": {
+                "UID": 4,
+                "Address": null,
+                "Name": "Tasker Homes ",
+                "Category/Layer": "Housing",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=11c0017eY2lfUQ6-Rb5miX5Zbu_HTNYoy",
+                    "https://drive.google.com/uc?export=view&id=1pIwc6RAvdwBofhC-1hM43ThOeVi5UlFW",
+                    "https://drive.google.com/uc?export=view&id=1K-YGTrRAMnyHsm7rVh_KU6OLTrH9-CEQ",
+                    "https://drive.google.com/uc?export=view&id=18QqSEQNmxD_yq3UzOeSwboRx7aWvkOjx",
+                    "https://drive.google.com/uc?export=view&id=1RUJPpodxgYwdhqzNpaPL-5JFVTepxUYn",
+                    "https://drive.google.com/uc?export=view&id=1besKNzRCZ4Sx0D1B1AIO8rR_9Jz1PYcu",
+                    "https://drive.google.com/uc?export=view&id=1Lm4cgF0ulukxH6asGg10dS3Kwraf_RZq"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1946337017502,
+                    39.932054331127
+                ]
+            },
+            "properties": {
+                "UID": 5,
+                "Address": null,
+                "Name": "Greater Grays Ferry Estate ",
+                "Category/Layer": "Housing",
+                "Contributed by": null,
+		"Audio": "audio/Asia_Greater_Grays_Ferry_Estate_Name.mp3",
+                "Video": null,
+                "Images": []
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1895740717001,
+                    39.9250347164112
+                ]
+            },
+            "properties": {
+                "UID": 6,
+                "Address": null,
+                "Name": "Wilson Park ",
+                "Category/Layer": "Housing",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1SSUCdxVouVH9dpoqoQ-tRs9jhmIuzoPz",
+                    "https://drive.google.com/uc?export=view&id=18orKG7_dVFPeMmxiFQTXiL2vtNCe8gH_",
+                    "https://drive.google.com/uc?export=view&id=1KhmwX7Idpk-0Lp4_mxUXjQPnG3_M-knx",
+                    "https://drive.google.com/uc?export=view&id=13YRBJeRvAAWLW19tZjE4Q6SvutYiIdJg",
+                    "https://drive.google.com/uc?export=view&id=1_z_Hbn5q0nHzf64rDtqUKhXBau864MCI"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.195875942546,
+                    39.9343610048058
+                ]
+            },
+            "properties": {
+                "UID": 7,
+                "Address": "3101 Tasker",
+                "Name": "Chucky Reeves ",
+                "Category/Layer": "Former Businesses",
+                "Contributed by": "Charles Reeves Jr",
+                "Audio": null,
+                "Video": null,
+                "Images": []
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1978026124119,
+                    39.9337259446664
+                ]
+            },
+            "properties": {
+                "UID": 8,
+                "Address": "3301 Tasker ",
+                "Name": "Audenried High School",
+                "Category/Layer": "Schools",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1_z_Hbn5q0nHzf64rDtqUKhXBau864MCI"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.19776956443,
+                    39.9351138315971
+                ]
+            },
+            "properties": {
+                "UID": 9,
+                "Address": "3200 Dickinson",
+                "Name": "James Alcorn School",
+                "Category/Layer": "Schools",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1LAuEIx50C6buFdegPMwCbPeO7q5olLx6",
+                    "https://drive.google.com/uc?export=view&id=1Xgo1ZLzC073PHrdVD9Gwm9hT5TUWnWyF",
+                    "https://drive.google.com/uc?export=view&id=1lo_dVN1piiQURcLPNGLsJmuN5JtzOvfC"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1921950796391,
+                    39.9371624303626
+                ]
+            },
+            "properties": {
+                "UID": 10,
+                "Address": "1308 S 29th ",
+                "Name": "Peace Wall",
+                "Category/Layer": "Public Space/Parks",
+                "Contributed by": "Rodney and Vilma Ray ",
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1MW5BLMzhicLmZxPqujI9kpvmiIetkBIG",
+                    "https://drive.google.com/uc?export=view&id=1dvs0ebfsIF__qrfX4rG-UdfoEoiBolH7"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1958314187124,
+                    39.9379513781177
+                ]
+            },
+            "properties": {
+                "UID": 11,
+                "Address": null,
+                "Name": "Origlio's Beer Distributor ",
+                "Category/Layer": "Former Businesses ",
+                "Contributed by": "Rodney and Vilma Ray ",
+                "Audio": null,
+                "Video": null,
+                "Images": []
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1891287327508,
+                    39.931783018971
+                ]
+            },
+            "properties": {
+                "UID": 12,
+                "Address": null,
+                "Name": "Vare Recreation Center ",
+                "Category/Layer": "Public Space/Parks",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1nyqKXTg5cxDJSXiFpU_2TTv84He49CbU"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.196581171394,
+                    39.9342748881131
+                ]
+            },
+            "properties": {
+                "UID": 13,
+                "Address": "3115 Tasker ",
+                "Name": "Cudden's Market",
+                "Category/Layer": "Former Businesses ",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1aLgCF2mugzIOuLLmy7exre2gJvPirjqA"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1967323249383,
+                    39.9200831634676
+                ]
+            },
+            "properties": {
+                "UID": 14,
+                "Address": null,
+                "Name": "Oil Refinery (PES Oil Refinery, Sunoco, Atlantic Refining Co) ",
+                "Category/Layer": "Environmental Impacts",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1XeBep19RuO281WaK1fl2iWQk-tVsXzGk",
+                    "https://drive.google.com/uc?export=view&id=1kUFxUElv6Qi0L0IIR20afl-jd1jS2spC",
+                    "https://drive.google.com/uc?export=view&id=1QkaxPdwZU-mf5pNnlYSyKGcIxJAEZwdK"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.2054739480809,
+                    39.9413809811614
+                ]
+            },
+            "properties": {
+                "UID": 15,
+                "Address": null,
+                "Name": "Grays Ferry Bridge",
+                "Category/Layer": "Streets ",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=1pkrJltnLc_jx9QS1AOzt38bT99LeJdL6"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.197551964589,
+                    39.9344111332866
+                ]
+            },
+            "properties": {
+                "UID": 16,
+                "Address": null,
+                "Name": "32nd and Tasker",
+                "Category/Layer": "Streets",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": [
+                    "https://drive.google.com/uc?export=view&id=17BTfX-bSsJhWiy7bargZ4ieKa8fE3II8"
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -75.1999408008989,
+                    39.9407219701947
+                ]
+            },
+            "properties": {
+                "UID": 17,
+                "Address": null,
+                "Name": "DuPont Paint Plant ",
+                "Category/Layer": "Environmental Impacts",
+                "Contributed by": null,
+                "Audio": null,
+                "Video": null,
+                "Images": []
+            }
+        }
+    ]
+};
